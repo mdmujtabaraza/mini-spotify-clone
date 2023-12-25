48342e948e852da113662573b4d3a31184614c85
mdmujtabaraza <45493966+mdmujtabaraza@users.noreply.github.com>
2023-12-25 08:34:15 +0530

    Updated postcss plugins & Added .prettierrc file.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -6,7 +6,12 @@ const config = {
     //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
     tailwindcss(),
     //But others, like autoprefixer, need to run after,
+    require("postcss-nested"),
+    require("postcss-mixins"),
+    require("postcss-preset-env"),
     autoprefixer,
+    require("postcss-reporter"),
+    require("cssnano"),
   ],
 };
 
