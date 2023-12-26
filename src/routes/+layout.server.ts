b8e0d9b8b07697dacf5c3b9c472f08e57fdc7c2d
mdmujtabaraza <45493966+mdmujtabaraza@users.noreply.github.com>
2023-12-27 02:11:46 +0530

    Refreshing Expired Access Tokens

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,11 @@
 import type { LayoutServerLoad } from "./$types";
 import { SPOTIFY_BASE_URL } from "$env/static/private";
+import { redirect } from "@sveltejs/kit";
 
-export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
+export const load: LayoutServerLoad = async ({ cookies, fetch, url }) => {
   const accessToken = cookies.get("access_token");
+  const refresh_token = cookies.get("refresh_token");
+
   if (!accessToken) {
     return {
       user: null,
@@ -22,6 +25,17 @@ export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
       await profileRes.json();
     return {
       user: profile,
+    };
+  }
+  if (profileRes.status === 401 && refresh_token) {
+    // refresh the token and try again
+    const refreshRes = await fetch("/api/auth/refresh");
+
+    if (refreshRes.ok) {
+      throw redirect(307, url.pathname);
+    }
+    return {
+      user: null,
     };
   } else {
     return {
