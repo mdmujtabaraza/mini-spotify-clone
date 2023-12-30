ccc68d471fc7f9631f856fb2a53024b727a99c08
mdmujtabaraza <45493966+mdmujtabaraza@users.noreply.github.com>
2023-12-31 03:39:25 +0530

    Added assets folder, no-js in app.html & installed iconify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,22 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
 <input />
 <select>
-    <option>Option</option>
+  <option>Option</option>
 </select>
 <button>Click me</button>
 
 <style lang="scss">
-    h1 {
-        color: red;
-        font-size: functions.toRem(36);
-        @include breakpoint.up('md'){
-            color: green;
-        }
-        @include breakpoint.up('lg'){
-            color: blue;
-        }
+  h1 {
+    color: red;
+    font-size: functions.toRem(36);
+    @include breakpoint.up("md") {
+      color: green;
     }
+    @include breakpoint.up("lg") {
+      color: blue;
+    }
+  }
 </style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+</script>
+
+<Icon icon="mdi:home" style="color: red" />
 <a href="/api/auth/login">Login</a>
