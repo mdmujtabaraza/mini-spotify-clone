a68ad8988daa94df7bcf009f074617e930f6605f
mdmujtabaraza <45493966+mdmujtabaraza@users.noreply.github.com>
2023-12-31 04:22:38 +0530

    11. Created a Button Component

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import { Button } from "$components";
   import Icon from "@iconify/svelte";
 </script>
 
 <Icon icon="mdi:home" style="color: red" />
-<a href="/api/auth/login">Login</a>
+<!-- <a href="/api/auth/login">Login</a> -->
+
+<Button element="a" variant="outline">Sign Up</Button>
+<Button element="button">Login</Button>
