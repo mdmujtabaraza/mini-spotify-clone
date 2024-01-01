7ad7e5b2d558287698406e2e7b8f21738f09a8fd
mdmujtabaraza <45493966+mdmujtabaraza@users.noreply.github.com>
2024-01-02 01:09:18 +0530

    Created the Top Bar & Animated it On Scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,25 @@
   import type { LayoutData } from "./$types";
   import { Navigation } from "$components";
 
+  let topbar: HTMLElement;
+  let scrollY: number;
+  let headerOpacity = 0;
+
   export let data: LayoutData;
 
+  $: if (topbar) {
+    headerOpacity = Math.min(2, scrollY / topbar.offsetHeight);
+    // headerOpacity = scrollY / topbar.offsetHeight < 2 ? scrollY / topbar.offsetHeight : 2;
+    // console.log(headerOpacity);
+  }
+
+  // $: console.log(topbar && scrollY / topbar.offsetHeight);
   $: user = data.user;
 </script>
 
 <!-- {data.user?.display_name} -->
+
+<svelte:window bind:scrollY />
 
 <div id="main">
   {#if user}
@@ -18,7 +31,15 @@
     </div>
   {/if}
   <div id="content">
-    <main id="main-content">
+    <div id="topbar" bind:this={topbar}>
+      <div
+        class="topbar-bg"
+        style:background-color="var(--header-color)"
+        style:opacity={headerOpacity}
+      />
+      Topbar
+    </div>
+    <main id="main-content" class:logged-in={user}>
       <slot />
     </main>
   </div>
@@ -29,10 +50,34 @@
     display: flex;
     #content {
       flex: 1;
+      #topbar {
+        position: fixed;
+        height: var(--header-height);
+        padding: 0 15px;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        z-index: 100;
+        .topbar-bg {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          z-index: -1;
+        }
+        @include breakpoint.up("md") {
+          padding: 0 30px;
+          width: calc(100% - var(--sidebar-width));
+        }
+      }
       main#main-content {
         padding: 30px 15px 60px;
         @include breakpoint.up("md") {
           padding: 30px 30px 60px;
+        }
+        &.logged-in {
+          padding-top: calc(30px + var(--header-height));
         }
       }
     }
