d7c455c7848b2aa0197b5af51575104912052087
mdmujtabaraza <45493966+mdmujtabaraza@users.noreply.github.com>
2024-01-03 19:19:30 +0530

    22. Created & Styled the User Menu Button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import { browser } from "$app/environment";
   import { Navigation } from "$components";
+  import { page } from "$app/stores";
+  import Icon from "@iconify/svelte";
+
+  $: user = $page.data.user;
 </script>
 
 <div class="content">
@@ -9,7 +13,21 @@
       <Navigation desktop={false} />
     {/if}
   </div>
-  <div class="right">Right</div>
+  <div class="right">
+    <div id="profile-button">
+      <button class="profile-button">
+        {#if user?.images && user.images.length > 0}
+          <img src={user.images[0].url} alt={user.display_name} />
+        {/if}
+        {user?.display_name} <span class="visually-hidden">Profile Menu</span>
+        <Icon
+          icon="lucide:chevron-down"
+          class="profile-arrow"
+          style="font-size:22px;"
+        />
+      </button>
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
@@ -18,9 +36,28 @@
     justify-content: space-between;
     align-items: center;
     width: 100%;
-    .left {
+  }
+  .profile-button {
+    // background: none;
+    // border: none;
+    // cursor: pointer;
+    border: 1px solid var(--border);
+    padding: 5px;
+    border-radius: 25px;
+    display: flex;
+    align-items: center;
+    color: var(--text-color);
+    :global(.profile-arrow) {
+      margin-left: 3px;
     }
-    .right {
+    img {
+      width: 28px;
+      height: 28px;
+      border-radius: 100%;
+      margin-right: 10px;
+    }
+    &:hover {
+      background-color: var(--accent-color);
     }
   }
 </style>
